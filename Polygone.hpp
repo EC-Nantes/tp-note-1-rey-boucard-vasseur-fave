ae6389f1b7b0d5c1fd0969006e836c61f5384eb3
François Vasseur <vasseurfrancois11@gmail.com>
2023-01-13 10:22:25 +0100

    re

--- Polygone.hpp
+++ Polygone.hpp
@@ -82,11 +82,9 @@ void Polygone<T>::translate(T x, T y){
 template <typename T>
 std::ostream& operator<<(std::ostream &output, Polygone<T> const &p)
 {
-    int nb_sommets = 0;
     output << "Le polygone a pour sommets : {" << std::endl;
     for(auto i = p.sommets_.begin(); i != p.sommets_.end(); i++){
-        output << "p" << nb_sommets << " : " << *i;
-        nb_sommets++;
+        output << "p" << i-p.sommets_.begin()+1 << " : " << *i;
     }
     output << "}";
     return output;
