bfb5ed74a4b4e84f88d933d78c928bd356c12258
François Vasseur <vasseurfrancois11@gmail.com>
2023-01-13 11:29:53 +0100

    Create files for parcelle types

---
+++ ZAU.hpp
@@ -0,0 +1,14 @@
+/*
+ * ZAU.hpp
+ *
+ *  Created on: 13 jan. 2023
+ *      Author: REY Maxime, BOUCARD Romain, FAVE Jonathan, VASSEUR Francois
+ */
+
+#ifndef ZAU_HPP
+#define ZAU_HPP 
+
+class ZAU{
+};
+
+#endif /*ZAU.HPP*/
