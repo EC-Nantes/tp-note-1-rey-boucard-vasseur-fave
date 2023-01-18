74c3a0516363befbb2e97e7bb13bcb3ad5dc6ea2
RomainBoucard <rom.boucard@gmail.com>
2023-01-18 13:52:08 +0100

    Modifications fonctions de test

--- main.cpp
+++ main.cpp
@@ -78,6 +78,7 @@ void testZU(){
     Polygone<int> Poly1(listeSommets);
     ZU<int> ZoneU(12, "Pierre Dupont", Poly1);
     std::cout << ZoneU.getPConstructible() << endl;
+    std::cout << ZoneU.getsurfaceConstructible() << endl;
     std::cout << ZoneU.getsurfaceConstruite() << endl;
 }
 
@@ -95,6 +96,7 @@ void testZA(){
     Polygone<int> Poly1(listeSommets);
     ZA<int> ZoneA(12, "Pierre Dupont", "Ble", Poly1);
     std::cout << ZoneA.getPConstructible() << endl;
+    std::cout << ZoneA.getsurfaceConstructible() << endl;
     std::cout << ZoneA.gettypeCulture() << endl;
 }
 
@@ -112,6 +114,7 @@ void testZAU(){
     Polygone<int> Poly1(listeSommets);
     ZAU<int> ZoneAU(12, "Pierre Dupont",  Poly1);
     std::cout << ZoneAU.getPConstructible() << endl;
+    std::cout << ZoneAU.getsurfaceConstructible() << endl;
 }
 
 int main() {
